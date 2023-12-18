555e2f67aaab53aa6a079ef51b6e618547ce91e0
Frostbite22 <landoulsiferes@outlook.com>
2023-12-18 21:18:11 +0100

    Adding non linearity

--- micrograd/nn.py
+++ micrograd/nn.py
@@ -11,18 +11,21 @@ class Module:
     
 class Neuron(Module):
   
-  def __init__(self, nin):
+  def __init__(self, nin,nonlin=True):
     self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
     self.b = Value(random.uniform(-1,1))
+    self.nonlin = nonlin
   
   def __call__(self, x):
     # w * x + b
     act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
-    out = act.tanh()
-    return out
+    return act.relu() if self.nonlin else act
   
   def parameters(self):
     return self.w + [self.b]
+  
+  def __repr__(self) -> str:
+    return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
 
 class Layer(Module):
   
@@ -35,12 +38,15 @@ class Layer(Module):
   
   def parameters(self):
     return [p for neuron in self.neurons for p in neuron.parameters()]
+  
+  def __repr__(self):
+    return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
 
 class MLP(Module):
   
   def __init__(self, nin, nouts):
     sz = [nin] + nouts
-    self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+    self.layers = [Layer(sz[i], sz[i+1],nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
   
   def __call__(self, x):
     for layer in self.layers:
